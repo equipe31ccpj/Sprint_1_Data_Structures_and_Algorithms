ed51eb25c26577b46f949125c6d664e8003f66cc
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-05-01 15:08:37 -0300

    feat: implementar fluxo de autenticação de usuários

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import time
 
 usuarios = []
 
@@ -28,15 +30,15 @@ if escolha_usuario == 2:
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print('PERFIL DO USUÀRIO')
 	print('=~' * 20)
-	email = input('Digite seu e-mail: ')
+	criar_email = input('Digite seu e-mail: ')
 	tipo_conta = input('Digite o tipo da sua conta: ')
-	senha = input('Digite a senha: ')
+	criar_senha = input('Digite a senha: ')
 	conf_senha = input('Digite novamente a senha: ')
 
-	if senha != conf_senha:
-		while senha != conf_senha:
+	if criar_senha != conf_senha:
+		while criar_senha != conf_senha:
 			print('Senhas diferentes! Corrija.')
-			senha = input('Digite a senha: ')
+			entrar_senha = input('Digite a senha: ')
 			conf_senha = input('Digite novamente a senha: ')
 
 	os.system('cls' if os.name == 'nt' else 'clear')
@@ -47,14 +49,52 @@ if escolha_usuario == 2:
 	cod_verificacao = input('Digite o código de verificação: ')
 	tipo_estacao = input('Digite o tipo de estação: ')
 
-	usuario['e-mail'] = email
+	usuario['email'] = criar_email
 	usuario['tipo de conta'] = tipo_conta
-	usuario['senha'] = senha
+	usuario['senha'] = criar_senha
 	usuario['endereço'] = endereco
 	usuario['número de série'] = num_serie
 	usuario['código de verificação'] = cod_verificacao
 	usuario['tipo de estação'] = tipo_estacao
 	usuarios.append(usuario)
-
+	print('Conta criada com sucesso!')
+	time.sleep(5)	
 else:
-	print(1)		
+	pass
+
+entrar = False
+
+
+while not entrar:
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print('Entrar na conta.')
+	entrar_email = input('Digite seu e-mail: ')
+	entrar_senha = input('Digite a senha: ')
+
+	conf_entrar_email = False
+	conf_entrar_senha = False
+
+	for usuario in usuarios:
+		if entrar_email == usuario['email']:
+			conf_entrar_email = True
+			if entrar_senha == usuario['senha']:
+				conf_entrar_senha = True
+				break
+	if conf_entrar_email == True and conf_entrar_senha == True:
+		entrar = True
+	else:		
+		encerrar = False	
+		while not encerrar:
+			novamente = input('e-mail ou senha incorretas! Deseja tentar novamente: ')
+			if novamente.lower() == 'sim':
+				break
+			elif novamente.lower() == 'não':
+				print('Saindo do sistema.')
+				time.sleep(5)
+				sys.exit()
+			else:
+				print('repsosta não válida, tente novamente.')
+				time.sleep(5)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print('Entrada bem sucedida!')
